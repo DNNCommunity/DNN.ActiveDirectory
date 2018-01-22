5077a40ebb25a04ee1d51d5d1610b34c85918326
Steven West <sawest@iccms.edu>
2018-01-22 09:17:42 -0600

    version 6.0.6
    bug fix for #29

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -29,7 +29,7 @@ Imports System.Runtime.InteropServices
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("06.00.04.7")> 
+' <Assembly: AssemblyVersion("06.00.06.1")> 
 
-<Assembly: AssemblyVersion("06.00.04.7")>
-<Assembly: AssemblyFileVersion("06.00.04.7")>
+<Assembly: AssemblyVersion("06.00.06.1")>
+<Assembly: AssemblyFileVersion("06.00.06.1")>
