33c94a7da7b53663726dcd7092ce80b23f9c169b
West, Steven A <sawest@iccms.edu>
2025-02-04 22:41:21 -0800

    Adding startup scope

--- StartUp.vb
+++ StartUp.vb
@@ -11,6 +11,7 @@ Public Class Startup
         services.AddScoped(Of ADSI.Utilities)()
         services.AddScoped(Of Authentication.AuthenticationController)()
         services.AddScoped(Of UserController)()
+        services.AddScoped(Of GroupController)()
         services.AddScoped(Of ADSI.ADSIProvider)()
     End Sub
 
