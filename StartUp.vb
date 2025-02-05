3ca30a5e1828edbe1a18fc25132036bea7645e9e
West, Steven A <sawest@iccms.edu>
2025-02-04 19:23:14 -0800

    Added startup.vb and added dependency injection references.

---
+++ StartUp.vb
@@ -0,0 +1,15 @@
+﻿Imports DotNetNuke.DependencyInjection
+Imports Microsoft.Extensions.DependencyInjection
+
+Public Class Startup
+    Implements IDnnStartup
+
+    Public Sub ConfigureServices(services As IServiceCollection) Implements IDnnStartup.ConfigureServices
+        services.AddScoped(Of Configuration)()
+        services.AddScoped(Of DotNetNuke.Authentication.ActiveDirectory.ADSI.Configuration)()
+        services.AddScoped(Of DotNetNuke.Authentication.ActiveDirectory.ADSI.Utilities)()
+        services.AddScoped(Of Authentication.AuthenticationController)()
+        services.AddScoped(Of DotNetNuke.Authentication.ActiveDirectory.ADSI.ADSIProvider)()
+    End Sub
+
+End Class
